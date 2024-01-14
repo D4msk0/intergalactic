53690d3a8452ac833f3585542d28a5cf00001b02
Merry Damsko <m.d4msk0@gmail.com>
2024-01-15 00:52:47 +0100

    added thymeleaf dep to pom

---
+++ src/main/java/com/damsko/intergalactic/IntergalacticApplication.java
@@ -0,0 +1,13 @@
+package com.damsko.intergalactic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergalacticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergalacticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damsko/intergalactic/IntergalacticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damsko.intergalactic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntergalacticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
