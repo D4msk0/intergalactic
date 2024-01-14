561cacb72cf4d574999e4c2246f3ef7dec8f7aac
Merry Damsko <m.d4msk0@gmail.com>
2024-01-15 00:56:15 +0100

    added mapping to controller

--- src/main/java/com/damsko/intergalactic/controller/IntergalacticController.java
+++ src/main/java/com/damsko/intergalactic/controller/IntergalacticController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IntergalacticController {
 
-    @GetMapping
+    @GetMapping("/")
     public String intergalactic(){
         return "intergalactic";
     }
